99635abba7da4bff1165aa3e4e62a2cf7701cd89
Нагаев Владислав <nagaev.vl@OB.LOCAL>
2024-07-18 11:46:52 +0700

    урлы/вьюхи - прошёл

--- GDFR/Low_skill_proj/blog/urls.py
+++ GDFR/Low_skill_proj/blog/urls.py
@@ -1,13 +1,42 @@
-from django.urls import path, re_path
-from .views import *
+from django.urls import path, include, re_path
+from blog.views import *
+
 
 
 urlpatterns = [
-    # path('', homepageview, name='home'),
-    path('user/<slug:name>/<int:age>/', user,),
-    path('user/<slug:name>/', user,),
-    path('user/', user,),
-    re_path(r'^about/', about, kwargs={'name': 'Valera', 'age': '44'}),
-    re_path(r'^contact/', contact),
-    path('', index, name='main'),
+    path("index/<int:ident>/", index),
+    path("access/<int:age>/", access),
 ]
+
+
+# urlpatterns = [
+#     path('', index),
+#     path('about/', about),
+#     path('contact/', contact),
+#     path('details/', details)
+# ]
+
+# product_patterns = [
+#     path('', products),
+#     path('comments/', comments),
+#     path('question/', question)
+# ]
+
+# urlpatterns = [
+#     path('', index),
+#     path('products/<int:id>/', include(product_patterns))
+# ]
+
+# urlpatterns = [
+#     # path('', homepageview, name='home'),
+#     # path('user/<str:name>/<int:age>/', user),
+#     # path('user/<str:name>/', user),
+#     path('', index, name='main'),
+#     re_path(r'^contact/', contact),
+#     re_path(r'^about/', about, kwargs={'name': 'Valera', 'age': '44'}),
+#     re_path(r'^user/(?P<name>\D+)/(?P<age>\d+)/', user),
+#     re_path(r'^user/(?P<name>\D+)/', user),
+#     path('user/', user,),
+    
+    
+# ]
--- GDFR/Low_skill_proj/blog/views.py
+++ GDFR/Low_skill_proj/blog/views.py
@@ -1,25 +1,83 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseBadRequest, HttpResponseForbidden
 
-# Create your views here.
+
+
+
+def index(request, ident):
+    people = [None, 'Bob', 'Sam', 'Tom']
+    if ident in range(1, len(people)):
+        return HttpResponse(people[ident])
+    else:
+        return HttpResponseNotFound('Пользователь не найден')
+    
+def access(request, age):
+    if age not in range(1, 111):
+        return HttpResponseBadRequest('Не корректные данные')
+    if (age>17):
+        return HttpResponse('Доступ разрещён')
+    else:
+        return HttpResponseForbidden('Доступ заблокирован - слишком юн')
+    
+
+
+
+
+
+# def index(request):
+#     return HttpResponse('Нету', status=404)
+
+# def index(request):
+#     return HttpResponseNotFound('Нет такой страницы')
+
+# def index(request):
+#     return HttpResponse('main')
+
+# def about(request):
+#     return HttpResponse('About')
+
+# def contact(request):
+#     return HttpResponseRedirect('/about')
+
+# def details(request):
+#     return HttpResponsePermanentRedirect('/')
+
+# def index(request):
+#     return HttpResponse('Главаня страница')
+
+# def user(request):
+#     age = request.GET.get('age', 0)
+#     name = request.GET.get('name', 'Undefined')
+#     return HttpResponse(f'<h2>Имя: {name}, Возвраст: {age}</h2>')
+
+# def products(request, id):
+#     return HttpResponse('Товар')
+
+# def comments(request, id):
+#     return HttpResponse('Комментарии о товаре')
+
+# def question(request, id):
+#     return HttpResponse('Вопросы о товаре')
+
 
 # def homepageview(request):
 #     return HttpResponse('Hello, world!')
 
-def index (request):
-    return HttpResponse('''<h2>Главная</h2><br>
-                        <a href="http://127.0.0.1:8000/about">О пользователе</a><br>
-                        <a href="http://127.0.0.1:8000/contact">Контакты</a><br>
-                        <a href="http://127.0.0.1:8000/user">Пользователь</a><br>''')
+# def index (request):
+#     return HttpResponse('''<h2>Главная</h2><br>
+#                         <a href="http://127.0.0.1:8000/about">О пользователе</a><br>
+#                         <a href="http://127.0.0.1:8000/contact">Контакты</a><br>
+#                         <a href="http://127.0.0.1:8000/user">Пользователь</a><br>''')
 
-def about(request, name, age):
-    return HttpResponse(f'''<h2>О пользователе</h2>
-                        <p>Имя: {name}</p>
-                        <p>Возвраст: {age}</p>
-                        ''')
+# def about(request, name, age):
+#     return HttpResponse(f'''<h2>О пользователе</h2>
+#                         <p>Имя: {name}</p>
+#                         <p>Возвраст: {age}</p>
+#                         ''')
 
-def contact(request):
-    return HttpResponse('<h2>Контакты</h2>')
+# def contact(request):
+#     return HttpResponse('<h2>Контакты</h2>')
 
-def user(request, name='Undefined', age=0):
-    return HttpResponse(f'<h2>Пользователь:</h2><br><h5>{name} : {age}</h5>')
+# def user(request, name='Undefined', age=0):
+#     return HttpResponse(f'<h2>Пользователь:</h2><br><h5>{name} : {age}</h5>')
